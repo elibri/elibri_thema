fa55612f4881d184744eab553cd7f8a6826e4c1f
Tomasz Meka <tomek@gildia.pl>
2019-10-06 19:07:20 +0200

    nie pokazuj norodowych celów edukacyjnych polskim wydawcom

--- lib/elibri_thema.rb
+++ lib/elibri_thema.rb
@@ -14,10 +14,20 @@ module ElibriThema
     end
   end
 
+
+  #nie interesują nas te poddrzewa - oszczędzamy czas ładowania, szybciej też będzie działać wyszukiwanie
+  def self.unused_subtrees
+    ["4Z-CA-", "4Z-CB-", "4Z-DE-", "4Z-DK-", "4Z-ES-", "4Z-FR-", "4Z-GB-", "4Z-IE-", "4Z-IT-", "4Z-US-", "4Z-AA-",
+     "4Z-AR-", "4Z-BO-", "4Z-CL-", "4Z-CO-", "4Z-CR-", "4Z-CU-", "4Z-DO-", "4Z-EC-", "4Z-FI-", "4Z-GT-", "4Z-HN-",
+     "4Z-MX-", "4Z-NI-", "4Z-NO-", "4Z-PA-", "4Z-PE-", "4Z-PY-", "4Z-SV-", "4Z-UY-", "4Z-VE-"]
+
+  end
+
+  #tych kodów nie da się przypisać do książek
   def self.blocked_codes
     ["XQ", "XA", "FY", "5A", "5H", "5J", "5L", "5P"] +
     ["6A", "6B", "6C", "6D", "6E", "6F", "6G", "6H", "6J", "6K", "6L", "6M", "6N", "6P", "6Q", "6R", "6S", "6T", "6U", "6V", "6W", "6X"] +
-    ["4C", "4G", "4L", "4T", "4Z"]
+    ["4C", "4G", "4L", "4T", "4Z", "4Z-PL-"]
   end
 
   def self.nested_categories
--- lib/elibri_thema/version.rb
+++ lib/elibri_thema/version.rb
@@ -1,3 +1,3 @@
 module ElibriThema
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
